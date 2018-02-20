bedf1b939d354206cb3db9a497684e9492f39b40
Kennedy Rose <kennedy@kennedyrose.com>
2018-02-20 15:12:24 -0600

    Refactor

--- src/prepend-rows.js
+++ src/prepend-rows.js
@@ -2,41 +2,10 @@ import { google } from 'googleapis'
 
 const sheets = google.sheets('v4')
 
-// https://developers.google.com/sheets/api/samples/rowcolumn#insert_an_empty_row_or_column
-
-function authorizeGoogleJWT(clientEmail, privateKey, scopes) {
-	return new google.auth.JWT(
-		clientEmail,
-		null,
-		privateKey,
-		['https://www.googleapis.com/auth/spreadsheets'],
-		null
-	)
-}
-
-function authenticateGoogleSheets(clientEmail, privateKey) {
-	return Promise.resolve(authorizeGoogleJWT(clientEmail, privateKey))
-}
-
-function prepend(params, options = {}) {
-	return new Promise((resolve, reject) => {
-		sheets.spreadsheets.batchUpdate(params, options, (err, response) => {
-			if (err) {
-				reject(err);
-			}
-			else {
-				resolve(response)
-			}
-		});
-	});
-}
-
 export default async function() {
-	let auth = await authenticateGoogleSheets(this.options.clientEmail, this.options.privateKey)
 	let sheetId = 0
 	const params = {
 		spreadsheetId: this.options.spreadsheetId,
-		auth: auth,
 		resource: {
 			requests: [
 				{
@@ -77,7 +46,22 @@ export default async function() {
 				}
 			],
 		},
-		auth
+		auth: new google.auth.JWT(
+			this.options.clientEmail,
+			null,
+			this.options.privateKey,
+			['https://www.googleapis.com/auth/spreadsheets'],
+			null
+		)
 	}
-	await prepend(params)
+	await new Promise((resolve, reject) => {
+		sheets.spreadsheets.batchUpdate(params, {}, (err, response) => {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(response)
+			}
+		})
+	})
 }
